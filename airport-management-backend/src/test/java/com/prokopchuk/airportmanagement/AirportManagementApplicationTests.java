5be903848764e9f27c9a0105615678d2c0db41ab
Roman Prokopchuk <ghost62727@gmail.com>
2023-03-13 14:04:50 +0200

    Comment testcase in SpringBootTest

--- airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/AirportManagementApplicationTests.java
+++ airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/AirportManagementApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AirportManagementApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+//  @Test
+//  void contextLoads() {
+//  }
 
 }

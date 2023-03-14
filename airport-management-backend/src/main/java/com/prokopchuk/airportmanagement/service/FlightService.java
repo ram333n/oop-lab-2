053b64bbbc56415dbc11b63987037248b21d25cc
Roman Prokopchuk <ghost62727@gmail.com>
2023-03-14 17:05:54 +0200

    ✨ Implement FlightService (#14)

--- airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/service/FlightService.java
+++ airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/service/FlightService.java
@@ -1,26 +1,39 @@
 package com.prokopchuk.airportmanagement.service;
 
 import com.prokopchuk.airportmanagement.model.Flight;
+import com.prokopchuk.airportmanagement.repository.FlightRepository;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class FlightService {
 
+  private final FlightRepository flightRepository;
+
+  @Autowired
+  public FlightService(FlightRepository flightRepository) {
+    this.flightRepository = flightRepository;
+  }
+
+  @Transactional
   public Flight saveFlight(Flight flight) {
-    return flight;
+    return flightRepository.save(flight);
   }
 
   public Optional<Flight> findFlightById(Long id) {
-    return Optional.of(new Flight());
+    return flightRepository.findById(id);
   }
 
+  @Transactional
   public Flight updateFlight(Flight flight) {
-    return flight;
+    return flightRepository.save(flight);
   }
 
+  @Transactional
   public boolean deleteFlightById(Long id) {
-    return true;
+    return flightRepository.deleteFlightById(id) > 0L;
   }
 
 }
--- airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/service/FlightServiceTest.java
+++ airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/service/FlightServiceTest.java
@@ -1,46 +1,110 @@
 package com.prokopchuk.airportmanagement.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.prokopchuk.airportmanagement.model.Flight;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
 
+@SpringBootTest
+@AutoConfigureTestDatabase
+@SqlGroup({
+    @Sql(scripts = "/clear.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(scripts = "/test-init.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+})
 class FlightServiceTest {
 
-  private final FlightService flightService = new FlightService();
+  @Autowired
+  private FlightService flightService;
 
   @Test
   void saveFlightWorksProperly() {
-    Flight toSave = new Flight();
+    LocalDateTime departureTime = LocalDateTime.now().plusHours(1L);
+    LocalDateTime arrivalTime = LocalDateTime.now().plusHours(6L);
 
-    Flight result = flightService.saveFlight(toSave);
+    Flight toSave = Flight.builder()
+        .departureFrom("Kyiv")
+        .destination("Zhytomyr")
+        .departureTime(departureTime)
+        .arrivalTime(arrivalTime)
+        .build();
 
-    assertNotNull(result);
+    Flight actual = flightService.saveFlight(toSave);
+
+    assertEquals(4L, actual.getId());
+    assertEquals("Kyiv", actual.getDepartureFrom());
+    assertEquals("Zhytomyr", actual.getDestination());
+    assertEquals(departureTime, actual.getDepartureTime());
+    assertEquals(arrivalTime, actual.getArrivalTime());
   }
 
   @Test
-  void findFlightByIdWorksProperly() {
-    Optional<Flight> result = flightService.findFlightById(1L);
+  void findFlightByIdWhenProvidedExistentId() {
+    //2023-05-23 23:53:00 - expected departure time
+    //2023-05-24 16:09:00 - expected arrival time
+    LocalDateTime expectedDepartureTime = LocalDateTime.of(2023, 5, 23, 23, 53);
+    LocalDateTime expectedArrivalTime = LocalDateTime.of(2023, 5, 24, 16, 9);
 
-    assertTrue(result.isPresent());
+    Optional<Flight> actual = flightService.findFlightById(1L);
+
+    assertTrue(actual.isPresent());
+    assertEquals("Kyiv", actual.get().getDepartureFrom());
+    assertEquals("Krakow", actual.get().getDestination());
+    assertEquals(expectedDepartureTime, actual.get().getDepartureTime());
+    assertEquals(expectedArrivalTime, actual.get().getArrivalTime());
+  }
+
+  @Test
+  void findFlightByIdWhenProvidedNonExistentId() {
+    Optional<Flight> actual = flightService.findFlightById(0L);
+
+    assertTrue(actual.isEmpty());
   }
 
   @Test
   void updateFlightWorksProperly() {
-    Flight toUpdate = new Flight();
+    LocalDateTime departureTime = LocalDateTime.now().plusHours(1L);
+    LocalDateTime arrivalTime = LocalDateTime.now().plusHours(6L);
 
-    Flight result = flightService.updateFlight(toUpdate);
+    Flight toUpdate = Flight.builder()
+        .id(1L)
+        .departureFrom("Lviv")
+        .destination("Zhytomyr")
+        .departureTime(departureTime)
+        .arrivalTime(arrivalTime)
+        .build();
 
-    assertNotNull(result);
+    Flight actual = flightService.updateFlight(toUpdate);
+
+    assertEquals(1L, actual.getId());
+    assertEquals("Lviv", actual.getDepartureFrom());
+    assertEquals("Zhytomyr", actual.getDestination());
+    assertEquals(departureTime, actual.getDepartureTime());
+    assertEquals(arrivalTime, actual.getArrivalTime());
   }
 
   @Test
-  void deleteFlightByIdWorksProperly() {
+  void deleteFlightByIdWhenProvidedExistentId() {
     boolean isDeleted = flightService.deleteFlightById(1L);
 
     assertTrue(isDeleted);
+  }
+
+  @Test
+  void deleteFlightByIdWhenProvidedNonExistentId() {
+    boolean isDeleted = flightService.deleteFlightById(0L);
+
+    assertFalse(isDeleted);
   }
 
 }

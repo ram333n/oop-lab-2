3a2684b95fcc0d90c14c591742b8659e1585b71b
Roman Prokopchuk <ghost62727@gmail.com>
2023-03-14 20:33:45 +0200

    Add Dockerfile TODO: debug it

--- airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/controller/CrewMemberController.java
+++ airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/controller/CrewMemberController.java
@@ -2,6 +2,7 @@ package com.prokopchuk.airportmanagement.controller;
 
 import com.prokopchuk.airportmanagement.service.CrewMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class CrewMemberController {
   @Autowired
   public CrewMemberController(CrewMemberService crewMemberService) {
     this.crewMemberService = crewMemberService;
+  }
+
+  @GetMapping("/test")
+  public String testEndpoint() {
+    return "Hello";
   }
 
 }

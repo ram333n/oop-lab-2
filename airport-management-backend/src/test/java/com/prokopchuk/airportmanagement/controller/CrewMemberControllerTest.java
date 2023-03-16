a86c72008a8eadcb78d0d5772d9ec39c90214d02
Roman Prokopchuk <ghost62727@gmail.com>
2023-03-16 11:09:31 +0200

    Add tests for POST, PUT, DELETE endpoints

--- airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/controller/CrewMemberControllerTest.java
+++ airport-management-backend/src/test/java/com/prokopchuk/airportmanagement/controller/CrewMemberControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.prokopchuk.airportmanagement.controller.dto.crewmember.CrewMemberInputDto;
 import com.prokopchuk.airportmanagement.controller.dto.crewmember.CrewMemberResponseDto;
 import com.prokopchuk.airportmanagement.controller.dto.crewmember.CrewMemberWithoutFlightsDto;
 import com.prokopchuk.airportmanagement.controller.dto.crewmember.CrewMembersListDto;
@@ -18,7 +19,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
@@ -89,6 +91,101 @@ class CrewMemberControllerTest {
     );
 
     assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void createCrewMemberWorksProperly() {
+    CrewMemberInputDto inputDto = new CrewMemberInputDto(
+        "Andrii",
+        "Prokopenko",
+        Position.NAVIGATOR
+    );
+
+    ResponseEntity<CrewMemberResponseDto> response = restTemplate.postForEntity(
+        "/crew-members",
+        inputDto,
+        CrewMemberResponseDto.class
+    );
+
+    CrewMemberResponseDto responseDto = response.getBody();
+
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    assertEquals(5L, responseDto.getId());
+    assertEquals("Andrii", responseDto.getName());
+    assertEquals("Prokopenko", responseDto.getSurname());
+    assertEquals(Position.NAVIGATOR, responseDto.getPosition());
+  }
+
+  @Test
+  void updateCrewMemberWhenProvidedNonExistentId() {
+    CrewMemberInputDto inputDto = new CrewMemberInputDto(
+        "Andrii",
+        "Prokopenko",
+        Position.NAVIGATOR
+    );
+
+    HttpEntity<CrewMemberInputDto> httpEntity = new HttpEntity<>(inputDto);
+    ResponseEntity<CrewMemberResponseDto> response = restTemplate.exchange(
+        "/crew-members/{crew-member-number}",
+        HttpMethod.PUT,
+        httpEntity,
+        CrewMemberResponseDto.class,
+        0L
+    );
+
+    assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void updateCrewMemberWhenProvidedExistentId() {
+    CrewMemberInputDto inputDto = new CrewMemberInputDto(
+        "Andrii",
+        "Prokopenko",
+        Position.NAVIGATOR
+    );
+
+    HttpEntity<CrewMemberInputDto> httpEntity = new HttpEntity<>(inputDto);
+    ResponseEntity<CrewMemberResponseDto> response = restTemplate.exchange(
+        "/crew-members/{crew-member-number}",
+        HttpMethod.PUT,
+        httpEntity,
+        CrewMemberResponseDto.class,
+        1L
+    );
+
+    CrewMemberResponseDto responseDto = response.getBody();
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals(1L, responseDto.getId());
+    assertEquals("Andrii", responseDto.getName());
+    assertEquals("Prokopenko", responseDto.getSurname());
+    assertEquals(Position.NAVIGATOR, responseDto.getPosition());
+  }
+
+  @Test
+  void deleteCrewMemberWhenProvidedNonExistentId() {
+    ResponseEntity<Void> response = restTemplate.exchange(
+        "/crew-members/{crew-member-number}",
+        HttpMethod.DELETE,
+        HttpEntity.EMPTY,
+        Void.class,
+        0L
+    );
+
+    assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void deleteCrewMemberWhenProvidedExistentId() {
+    ResponseEntity<Void> response = restTemplate.exchange(
+        "/crew-members/{crew-member-number}",
+        HttpMethod.DELETE,
+        HttpEntity.EMPTY,
+        Void.class,
+        1L
+    );
+
+    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
   }
 
 }

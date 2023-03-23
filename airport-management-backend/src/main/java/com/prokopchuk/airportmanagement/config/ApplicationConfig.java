8c86aa39b279edfa2dd1dfdffbaf2b6ced1f1596
Roman Prokopchuk <ghost62727@gmail.com>
2023-03-23 13:50:53 +0200

    Rename config package

--- airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/config/ApplicationConfig.java
+++ airport-management-backend/src/main/java/com/prokopchuk/airportmanagement/config/ApplicationConfig.java
@@ -1,11 +1,11 @@
-package com.prokopchuk.airportmanagement.configuration;
+package com.prokopchuk.airportmanagement.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfiguration {
+public class ApplicationConfig {
 
   @Bean
   public ModelMapper modelMapper() {
